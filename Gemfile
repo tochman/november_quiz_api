9af2b9df2ce2d78ffb3c689c5927f9da1e641726
gsim1903 <gavin@internetbusker.co>
2021-12-14 15:08:16 +0100

    scaffold new rails application. Add rspec

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.0'
+
+gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'rack-cors'
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "pry-rails"
+end
+
+group :development do
+  gem 'listen', '~> 3.3'
+  gem 'spring'
+end
+
---
+++ config/application.rb
@@ -0,0 +1,37 @@
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module NovemberQuizApi
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    config.api_only = true
+    
+   # Disable generation of helpers, assets, routing
+    # and specs for helpers, routes, controller and requests
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.skip_routes true
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.request_specs false
+    end
+  end
+end
