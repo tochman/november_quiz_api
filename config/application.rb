b5944d53c7503d986da6391466cfe2fc5bf69fe9
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-14 16:07:18 +0100

    set up factory bot set up shoulda matchers set up Rack cors set up response_json custom method

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -21,8 +21,8 @@ module NovemberQuizApi
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
-    
-   # Disable generation of helpers, assets, routing
+
+    # Disable generation of helpers, assets, routing
     # and specs for helpers, routes, controller and requests
     config.generators do |generate|
       generate.helper false
@@ -32,6 +32,17 @@ module NovemberQuizApi
       generate.routing_specs false
       generate.controller_specs false
       generate.request_specs false
+    end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: %i[get post put delete],
+                 expose: %w[access-token expiry token-type uid client],
+                 max_age: 0
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,9 +13,26 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+module ResponseJSON
+  def response_json
+    JSON.parse(response.body)
+  end
+end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+  config.include ResponseJSON
 end
